6d50ef595708105cdf9d1988313a042feea1eea5
dodieboy <dodieboy@users.noreply.github.com>
2021-05-23 12:39:22 +0800

    [Done] 7-live-reload

--- test/src/app/tab2/tab2.page.ts
+++ test/src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
+import { Photos, PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,7 +8,8 @@ import { PhotoService } from '../services/photo.service';
   styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-  constructor(public photoService: PhotoService) {}
+  constructor(public photoService: PhotoService,
+    public actionSheetController: ActionSheetController) {}
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
@@ -15,5 +17,27 @@ export class Tab2Page {
 
   async ngOnInit() {
     await this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo: Photos, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
   }
 }
